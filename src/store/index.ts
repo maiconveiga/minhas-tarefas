e419f4b557d9d0cd668e153f2d1e4d32248957a8
= <=>
2024-03-22 17:00:54 -0300

    Adicionado o filtro

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import tarefasReducer from './reducers/tarefa'
+import filtroReducer from './reducers/filtros'
 
 const store = configureStore({
   reducer: {
-    tarefas: tarefasReducer
+    tarefas: tarefasReducer,
+    filtro: filtroReducer
   }
 })
 
