01d1c7eb3178ba080788b9fa874ce01ba5ed9c80
= <=>
2024-03-22 10:39:15 -0300

    Conclusão da estilização condicional dos botões

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,28 +1,41 @@
 import Tarefa from '../../components/Tarefa'
 import { Container } from './style'
 
+const tarefas = [
+  {
+    titulo: 'TCC',
+    descricao: 'Acessar materiais',
+    prioridade: 'importante',
+    status: 'concluída'
+  },
+  {
+    titulo: 'NPS',
+    descricao: 'Plano de ação',
+    prioridade: 'urgente',
+    status: 'pendente'
+  },
+  {
+    titulo: 'Fisioterapia cotovelo',
+    descricao: 'Exercícios para desinchar o edema',
+    prioridade: 'urgente',
+    status: 'pendente'
+  }
+]
+
 const ListaDeTarefas = () => (
   <Container>
     <p>2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;</p>
     <ul>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
+      {tarefas.map((t) => (
+        <li key={t.titulo}>
+          <Tarefa
+            titulo={t.titulo}
+            prioridade={t.prioridade}
+            status={t.status}
+            descricao={t.descricao}
+          />
+        </li>
+      ))}
     </ul>
   </Container>
 )
